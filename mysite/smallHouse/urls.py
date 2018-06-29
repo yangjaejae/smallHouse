0eb1992565ea561173eb2b82b664ff334d9419cf
yangjaejae <undertakin@naver.com>
2018-06-29 13:22:18 +0900

    markar 수정

--- mysite/smallHouse/urls.py
+++ mysite/smallHouse/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('', views.mainView, name='mainView')
+    url('main', views.mainView, name='mainView'),
+    url('getAddr', views.getAddr, name='getAddress')
 ]
--- mysite/smallHouse/views.py
+++ mysite/smallHouse/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 from legacydb.models import SmallHouseRealestate, SmallHouseRealnews
+from django.http import JsonResponse
+from django.http import HttpResponse
+import json
 
 def mainView(request):
-    
+    print("main###############################################")
     news = SmallHouseRealnews.objects.using('legacydb').all()
 
     return render(request, 'main/main.html',{'news': news})
+
+def getAddr(request):
+    print("getAddr##################################################")
+    if request.method == "GET":
+            
+        word= request.GET['word']
+        addrList = SmallHouseRealestate.objects.using('legacydb').filter(location=word)
+        dataList = []
+        result = {}
+        for li in addrList:
+            temp = {}
+            temp['id'] = li.id
+            temp['name'] = li.name
+            temp['price'] = li.price
+            temp['fnd_year'] = li.fnd_year
+            temp['sold_date'] = li.sold_date
+            temp['location'] = li.location
+            temp['loc_num'] = li.loc_num
+            temp['loc_cd'] = li.loc_cd
+            temp['floor'] = li.floor
+            dataList.append(temp)
+        result['result'] = dataList
+    return HttpResponse(json.dumps(result), content_type="application/json")
